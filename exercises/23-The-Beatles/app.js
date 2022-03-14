873f487b21822b392a48a3dde29d7b1d59643733
55746 <Lawhead.gavin@live.jobcorps.org>
2022-03-14 18:13:04 +0000

    javascript excersises

--- exercises/23-The-Beatles/app.js
+++ exercises/23-The-Beatles/app.js
@@ -1,5 +1,12 @@
+function sing(line1){
+     line1 = ['let it be, ']
+    for(let i = 0; i < line1.length; line1 < 4){
 
+    }
+return line1
 
-//Your code above ^^^
-
-console.log(sing());
+        
+    }
+    
+    //Your code above ^^^
+    // console.log(sing());
